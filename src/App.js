760ec3695120aa9ca80d7116ccec5680f40fcf90
Jonathan Parker <jprkr21@gmail.com>
2022-03-18 19:30:11 -0500

    Adding target blank to resume

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Hero from './components/AboutMe';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Navbar from './components/Navbar';
+import AboutMe from './components/AboutMe';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<AboutMe />} />  
+        <Route path='/hero' element={<Hero />} />  
+        <Route path='/portfolio' element={<Portfolio />} />  
+        <Route path='/contact' element={<Contact />} />  
+        <Route path='/resume' element={<Resume />} />  
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
-export default App;
+/*
+Navbar (HERO)
+  Jonathan Parker
+  NavLink
+    About Me (HOMEPAGE) (HERO)
+      Photo
+      Short Bio
+    Portfolio
+      Project (x6)
+    Contact
+      Form with Fields for Name, Email, Message
+    Resume
+      Downloadable Resume
+      List of Proficiencies
+Content
+Footer
+  Text or Icon Links to GitHub, LinkedIn, & Stack Overflow or Twitter
+*/
